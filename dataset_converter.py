585ab4ae5ad9ac650f46935f36fc0baa6465f7b9
Habib <habibmohammed2001@gmail.com>
2018-08-09 04:58:29 -0700

    glsd

--- dataset_converter.py
+++ dataset_converter.py
@@ -16,6 +16,8 @@ for entry in json_data:
 			entry[key] = int(value)
 		elif key == 'latitude' or key == 'longtiude' or key == 'frequency' or key == 'maxERP' or key == 'antennaHeight_AGL':
 			entry[key] = float(value)
-results.write(str(json_data))
+json_data = str(json_data)
+json_data = json_data.replace("'",'"')
+results.write(json_data)
 print(type(results))
 results.close()
